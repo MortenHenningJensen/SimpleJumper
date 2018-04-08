28a13edaa28f4e2af768bab42041460f55d32508
NielsJustesen <niels449@hotmail.com>
2018-04-08 16:20:50 +0200

    Jumping added with tween

--- SimpleJumperGame/Assets/PlayerControls.cs
+++ SimpleJumperGame/Assets/PlayerControls.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerControls : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject jumpDestination;
+
+    [SerializeField]
+    private Ease jumpEase;
+
+    private Vector3 startPos;
+
+    private bool isJumping;
     Rigidbody myBody;
     public Vector3 myForward;
     public int jumpforce = 10;
@@ -12,6 +22,7 @@ public class PlayerControls : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        this.startPos = this.transform.position;
         myBody = GetComponent<Rigidbody>();
         myForward = new Vector3(-1, 0, 0);
         score = 0;
@@ -19,9 +30,15 @@ public class PlayerControls : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !this.isJumping)
         {
-            myBody.AddForce((myForward * jumpforce) + new Vector3(0, 8, 0), ForceMode.Impulse);
+            //myBody.AddForce((myForward * jumpforce) + new Vector3(0, 8, 0), ForceMode.Impulse);
+            this.transform.DOJump(this.jumpDestination.transform.position, 5, 0, 1f).SetEase(this.jumpEase).OnStart(()=> this.isJumping = true).OnComplete(()=> this.isJumping = false);
+        }
+
+        if (this.transform.position.y <= -5)
+        {
+            ResetPlayer();
         }
 	}
 
@@ -48,6 +65,11 @@ public class PlayerControls : MonoBehaviour {
             //transform.Rotate(new Vector3(0, -1.2f, 0));
         }
 
+    }
+
+    private void ResetPlayer()
+    {
+        this.transform.position = startPos;
     }
 
 }
