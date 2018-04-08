4f709a54e0f6988e5433500cec3a685e0a78cc69
Unknown <mortenhenningjensen@hotmail.com>
2018-04-08 14:35:53 +0200

    Prototype
    Base Prototype

---
+++ Jumper/Assets/Platform.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    [SerializeField]
+    private PlayerControls player;
+    [SerializeField]
+    private int scoreToAdd = 1;
+
+    private bool hitByPlayer;
+
+    int minZvalue = -20;
+    int maxZvalue = 20;
+
+    public int movespeed = 20;
+
+    public int resetPos = -20;
+
+    public bool direction;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        gameObject.transform.Translate(new Vector3(0, 0, movespeed) * Time.deltaTime);
+
+        if (movespeed < 0)
+        {
+            direction = false;
+        }
+        else
+        {
+            direction = true;
+        }
+
+
+        if (direction)
+        {
+            if (gameObject.transform.position.z >= maxZvalue)
+            {
+                gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, resetPos);
+            }
+
+        }
+        else
+        {
+            if (gameObject.transform.position.z <= minZvalue)
+            {
+                gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, resetPos);
+            }
+
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerControls>() && !hitByPlayer)
+        {
+            hitByPlayer = true;
+            //  player.score += scoreToAdd;
+        }
+    }
+}
---
+++ Jumper/Assets/PlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    Rigidbody myBody;
+    public Vector3 myForward;
+    public int jumpforce = 20;
+
+    public int score;
+
+	// Use this for initialization
+	void Start () {
+        myBody = GetComponent<Rigidbody>();
+        myForward = new Vector3(-1, 0, 0);
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.AddForce((myForward * jumpforce) + new Vector3(0, 8, 0), ForceMode.Impulse);
+        }
+	}
+
+    public void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = collision.transform;
+            //transform.Rotate(new Vector3(0, -1.2f, 0));
+        }
+        else
+        {
+            Debug.Log(collision);
+
+            transform.parent = null;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+            //transform.Rotate(new Vector3(0, -1.2f, 0));
+        }
+
+    }
+
+}
