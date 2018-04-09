4143f6a73af3445755ccfbede3e6ea5990048a6a
NielsJustesen <niels449@hotmail.com>
2018-04-09 17:29:43 +0200

    ObjectPool work in progress

--- SimpleJumperGame/Assets/ObjectPool.cs
+++ SimpleJumperGame/Assets/ObjectPool.cs
@@ -6,8 +6,10 @@ public class ObjectPool : MonoBehaviour
 {
     public static ObjectPool Instance;
 
+    [SerializeField]
     private List<GameObject> enabledObjects = new List<GameObject>();
 
+    [SerializeField]
     private List<GameObject> disabledObjects = new List<GameObject>();
 
 	void Awake ()
@@ -15,5 +17,43 @@ public class ObjectPool : MonoBehaviour
         Instance = this;
 	}
 	
-    
+    public GameObject Instantiate(GameObject gameObject, Vector3 position, Quaternion rotation)
+    {
+        GameObject goToInstantiate = gameObject;
+        if (disabledObjects.Contains(gameObject))
+        {
+            goToInstantiate.transform.position = position;
+            goToInstantiate.transform.rotation = rotation;
+
+            disabledObjects.Remove(goToInstantiate);
+
+            goToInstantiate.SetActive(true);
+
+            enabledObjects.Add(goToInstantiate);
+
+            return goToInstantiate;
+        }
+        else 
+        {
+            goToInstantiate = Instantiate(gameObject, position, rotation);
+            enabledObjects.Add(goToInstantiate);
+
+            Debug.LogWarning("GameObject:" + gameObject.name +" wasn't in the disableObjects list, and was instantiated the old way");
+
+            return goToInstantiate;
+        }
+    }
+
+    public void Destroy(GameObject gameObject)
+    {
+        if (enabledObjects.Contains(gameObject))
+        {
+            gameObject.SetActive(false);
+            disabledObjects.Add(gameObject);
+        }
+        else
+        {
+            Debug.LogWarning("The gameObject: " + gameObject.name + " was not in the pool, and could not be destroyed");
+        }
+    }
 }
---
+++ SimpleJumperGame/Assets/ObjectPoolTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolTest : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject testObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("T was pressed");
+            //GameObject test = ObjectPool.Instance.Instantiate(testObject, new Vector3(0, 8, 0), Quaternion.identity);
+        }
+	}
+}
