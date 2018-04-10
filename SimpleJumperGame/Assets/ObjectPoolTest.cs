db297d8cc6e643a109c89b6a14bf65ba97651651
NielsJustesen <niels449@hotmail.com>
2018-04-10 18:13:33 +0200

    Object pool spawn working, ty Brackeys

--- SimpleJumperGame/Assets/ObjectPoolTest.cs
+++ SimpleJumperGame/Assets/ObjectPoolTest.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPoolTest : MonoBehaviour {
+public class ObjectPoolTest : MonoBehaviour
+{
+    PoolManager objectPool;
 
+    // Use this for initialization
+    void Start()
+    {
+        objectPool = PoolManager.Instance;
+    }
 
-    [SerializeField]
-    GameObject testObject;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Debug.Log("T was pressed");
-            //GameObject test = ObjectPool.Instance.Instantiate(testObject, new Vector3(0, 8, 0), Quaternion.identity);
+            objectPool.SpawnObject("Cube", transform.position, Quaternion.identity);
         }
-	}
+    }
 }
---
+++ SimpleJumperGame/Assets/PoolManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+    [SerializeField]
+    private Transform poolsParent;
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static PoolManager Instance;
+
+    public List<Pool> pools;
+
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.transform.parent = poolsParent;
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnObject(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("The pool with tag: " + tag + " doesnt exist");
+            return null;
+        }
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+}
