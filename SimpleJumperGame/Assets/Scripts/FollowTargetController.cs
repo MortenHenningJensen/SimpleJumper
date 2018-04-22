437c85327a9170bef86170a3c1915f07b08d0839
Unknown <mortenhenningjensen@hotmail.com>
2018-04-22 15:33:12 +0200

    Map update
    Made a change to FollowTarget, so the walls will stay at their X position, so it looks much better.

--- SimpleJumperGame/Assets/Scripts/FollowTargetController.cs
+++ SimpleJumperGame/Assets/Scripts/FollowTargetController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum followType { XZ, X}
 public class FollowTargetController : MonoBehaviour
 {
     [SerializeField]
     private GameObject target;
+
+    [SerializeField]
+    private followType ft;
 
     private Vector3 offset;
 
@@ -16,6 +20,15 @@ public class FollowTargetController : MonoBehaviour
 	
 	void Update ()
     {
-        this.transform.position = new Vector3(this.target.transform.position.x + this.offset.x, this.transform.position.y,  this.target.transform.position.z + this.offset.z);
+        switch (ft)
+        {
+            case followType.XZ:
+                this.transform.position = new Vector3(this.target.transform.position.x + this.offset.x, this.transform.position.y, this.target.transform.position.z + this.offset.z);
+                break;
+            case followType.X:
+                this.transform.position = new Vector3(this.target.transform.position.x + this.offset.x, this.transform.position.y, this.transform.position.z);
+                break;
+        }
+
 	}
 }
