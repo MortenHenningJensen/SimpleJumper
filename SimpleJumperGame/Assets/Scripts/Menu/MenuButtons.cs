db406c2f72b3d9b3f9f0a5fe7e6d45c1c5fbf828
Unknown <mortenhenningjensen@hotmail.com>
2018-04-28 13:47:13 +0200

    MainMenu start made
    Set up Start, store and exit
    
    Store gets how many coins you have, and displays a 3d model.
    
    removed some unused models

---
+++ SimpleJumperGame/Assets/Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text coinText;
+
+    private int myCoinNum;
+
+    [SerializeField]
+    private Canvas shopCanvas;
+    [SerializeField]
+    private Canvas menuCanvas;
+
+
+    [SerializeField]
+    private Camera menuCam;
+    [SerializeField]
+    private Camera shopCam;
+
+    private void Start()
+    {
+        shopCam.enabled = false;
+        myCoinNum = PlayerPrefs.GetInt("myCoins");
+        coinText.text = myCoinNum.ToString();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenShop()
+    {
+        shopCanvas.enabled ^= true;
+        menuCanvas.enabled ^= true;
+
+        if (shopCanvas.enabled == true)
+        {
+            menuCam.enabled = false;
+            shopCam.enabled = true;
+        }
+        else
+        {
+            menuCam.enabled = true;
+            shopCam.enabled = false;
+        }
+
+    }
+}
